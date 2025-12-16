d7f2c556c6f438c3592dc12bc3122e486f5d4446
LessVegetables <57289239+LessVegetables@users.noreply.github.com>
2025-12-16 14:07:38 +0700

    Updated genai4 url

--- routes.js
+++ routes.js
@@ -4,6 +4,6 @@ window.REDIRECT_ROUTES = Object.freeze({
     genai4: {
         name: "GenAI 4",
         description: "Проектная деятельность 25-26",
-        url: "https://yourcreative.streamlit.app",
+        url: "https://genai4project-aqmfzjftc6ul2usdeudg54.streamlit.app",
     },
 });
