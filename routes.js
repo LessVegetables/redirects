d6754df3348f515ef9b1ab00e9f5fefb3a1af40f
LessVegetables <57289239+LessVegetables@users.noreply.github.com>
2026-05-16 14:09:32 +0700

    Add Google Spreadsheet route to routes.js

--- routes.js
+++ routes.js
@@ -6,4 +6,9 @@ window.REDIRECT_ROUTES = Object.freeze({
         description: "Проектная деятельность 25-26",
         url: "https://genai4project-aqmfzjftc6ul2usdeudg54.streamlit.app",
     },
+    myGoogleSpreadsheet: {
+        name: "My Google Spreadsheet",
+        description: "Таблица баллов весна 26",
+        url: "https://docs.google.com/spreadsheets/d/19_klhVPMM0vVxOSFrM5CaoSEotCUSyMQjLkMma0py2A/",
+    },
 });
