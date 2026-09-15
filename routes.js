1c6112e7f81ce32437c52c4281f969c6c3a802f6
LessVegetables <57289239+LessVegetables@users.noreply.github.com>
2026-09-15 12:40:09 +0700

    added 5 gh routes for jarvis project

--- routes.js
+++ routes.js
@@ -6,4 +6,29 @@ window.REDIRECT_ROUTES = Object.freeze({
         description: "Проектная деятельность 25-26",
         url: "https://genai4project-aqmfzjftc6ul2usdeudg54.streamlit.app",
     },
+    fedor_repo: {
+        name: "Fedor's repo",
+        description: "GitHub: FedorSolodkin",
+        url: "https://github.com/FedorSolodkin",
+    },
+    stepan_repo: {
+        name: "Stepan's repo",
+        description: "GitHub: dext01",
+        url: "https://github.com/dext01",
+    },
+    daniil_repo: {
+        name: "Daniil's repo",
+        description: "GitHub: sheelestun/Edge_NSU_b_part",
+        url: "https://github.com/sheelestun/Edge_NSU_b_part",
+    },
+    daniel_repo: {
+        name: "Daniel's repo",
+        description: "GitHub: lessvegetables",
+        url: "https://github.com/lessvegetables/",
+    },
+    gleb_repo: {
+        name: "Gleb's repo",
+        description: "GitHub: gvbelavin",
+        url: "https://github.com/gvbelavin",
+    },
 });
